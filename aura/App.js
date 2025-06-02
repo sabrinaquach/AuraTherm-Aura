676a1b9b1e04174af8fcf36d14bdc48cdb96c8b2
sabrina <158983637+sabrinaquach@users.noreply.github.com>
2025-06-02 12:27:45 -0700

    push

--- aura/App.js
+++ aura/App.js
@@ -2,6 +2,10 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { LinearGradient} from 'expo-linear-gradient';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import Onboarding from '/Users/sabrina/AuraTherm-Aura/aura/screens/onboarding';
 
 export default function App() {
   return (
