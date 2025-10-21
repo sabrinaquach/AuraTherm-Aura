53e27f13001aeecb579829c9ba03b244aa49c735
Grikee <152575528+Grikee@users.noreply.github.com>
2025-10-20 20:06:30 -0700

    Update ThermostatAPI.h

--- Arduino/ThermostatAPI.h
+++ Arduino/ThermostatAPI.h
@@ -1,10 +1,25 @@
 #ifndef THERMOSTATAPI_H
 #define THERMOSTATAPI_H
 
+#include <Arduino.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_BME280.h>
+#include <Wire.h>
 #include <WebServer.h>
 
+// Web server
 extern WebServer server;
 
-void setupAPI();  // Initialize API endpoints
+// Temperature sensor
+extern Adafruit_BME280* bme;
+extern float currentTemp;
+extern int targetTemp;
+
+// Functions
+void setupAPI();
+void handleStatus();
+void handleSetTemp();
+void updateCurrentTemp();
+void temp_setup();
 
 #endif
